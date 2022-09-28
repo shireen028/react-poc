a9abdd4c188e57a7adc26b2e86fc2f6c808678f6
shireen028 <shireen@gleecus.com>
2022-09-28 13:55:23 +0530

    Update: colors

--- src/components/Button/elements.jsx
+++ src/components/Button/elements.jsx
@@ -1,15 +1,16 @@
-import { Button } from 'antd';
-import styled from 'styled-components';
+import { Button } from "antd";
+import styled from "styled-components";
 
 export const StyledButton = styled(Button)`
-  background: #3e6bff;
+  //background: #3e6bff;
+  background: #0030cc;
   border: none;
   color: #fff;
   height: 45px;
   width: 100%;
 
-  &:hover{
-    background: #3155CC;
+  &:hover {
+    background: #3155cc;
     border: none;
     color: #fff;
   }
