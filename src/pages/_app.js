8812dab59dcec739cf25078be3d303654a3b6571
shireen028 <shireen@gleecus.com>
2022-09-29 18:06:56 +0530

    fix menu

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,23 +1,24 @@
-import 'antd/dist/antd.css'; // or 'antd/dist/antd.less'
-import { Layout } from 'antd';
+import "antd/dist/antd.css"; // or 'antd/dist/antd.less'
+import { Layout } from "antd";
 const { Content } = Layout;
 
-import { Header, Footer } from '../components';
-import Head from 'next/head';
+import { Header, Footer } from "../components";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
-          rel='preconnect'
-          href='https://fonts.gstatic.com'
-          crossOrigin='true'
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
         />
         <link
-          href='https://fonts.googleapis.com/css2?family=Nunito+Sans&display=swap'
-          rel='stylesheet'
+          href="https://fonts.googleapis.com/css2?family=Nunito+Sans&display=swap"
+          rel="stylesheet"
         />
       </Head>
       <Layout style={{ fontFamily: "'Nunito Sans', sans-serif" }}>
